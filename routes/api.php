055d6b24ba353859f355814722969a56e9b35b49
jp <jp.lozano.sa@gmail.com>
2021-04-16 12:22:31 -0600

    Added nominated

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\JournalController;
 use App\Http\Controllers\PaperController;
 use App\Http\Controllers\AssignedsController;
+use App\Http\Controllers\NominatedReviewersController;
 
 /*
 |--------------------------------------------------------------------------
