f93753db3661cfb5fc04623e761a06719fe9ede0
Mohammad Al Sharbati <m.alsharbati97@hotmail.com>
2021-04-18 20:13:04 -0600

    Fixed errors.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,7 +12,8 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
 
-    public function createToken(Request $request) {
+    public function createToken(Request $request)
+    {
 
         $validated = $request->validate([
             'tokenName' => 'required|string',
@@ -24,7 +25,8 @@ class UserController extends Controller
         ]);
     }
 
-    public function deleteToken(Request $request) {
+    public function deleteToken(Request $request)
+    {
 
         $validated = $request->validate([
             'tokenId' => 'required|integer',
@@ -35,16 +37,18 @@ class UserController extends Controller
         ]);
     }
 
-    public function getTokens(Request $request) {
+    public function getTokens(Request $request)
+    {
         return response()->json([
-            'tokens'=>$request->user()->tokens
+            'tokens' => $request->user()->tokens
         ]);
     }
 
     /**
      * Handle user registration
      */
-    public function register(Request $request) {    
+    public function register(Request $request)
+    {
         $validated = $request->validate([
             'email' => 'email|required|unique:users',
             'password' => 'required|string|min:6',
@@ -54,18 +58,18 @@ class UserController extends Controller
             'type' => 'required|in:viewer,researcher,reviewer',
             'degrees' => 'required_if:type,researcher|array',
         ]);
-        
-        if ( $request->type === "researcher" ) {
+
+        if ($request->type === "researcher") {
             $index = 0;
             $errors = [];
-            foreach ( $request->degrees as $degree) {
+            foreach ($request->degrees as $degree) {
                 $validator = Validator::make($degree, [
-                    'title'=>'required|string',
-                    'institution'=>'required|string',
-                    'received'=>'required|date'
+                    'title' => 'required|string',
+                    'institution' => 'required|string',
+                    'received' => 'required|date'
                 ]);
-                if ( $validator->fails()) 
-                    $errors['degree_'.$index] = 
+                if ($validator->fails())
+                    $errors['degree_' . $index] =
                         $validator->errors();
 
                 $index++;
@@ -73,13 +77,12 @@ class UserController extends Controller
 
             if (count($errors) > 0) {
                 return response()->json([
-                    "message"=>"The given data was invalid",
-                    "errors"=> $errors
+                    "message" => "The given data was invalid",
+                    "errors" => $errors
                 ], 422);
-            }      
-
+            }
         }
-        
+
         $user = new User();
         $user->email = $request->email;
         $user->password = Hash::make($request->password);
@@ -88,17 +91,17 @@ class UserController extends Controller
         $user->last_name = $request->last_name;
 
         if ($request->type == 'viewer')
-            $user->status ='approved';
+            $user->status = 'approved';
         else
-            $user->status ='awaiting';
+            $user->status = 'awaiting';
 
 
         $user->save();
-        
-        if ($request->type == "researcher"){
+
+        if ($request->type == "researcher") {
             $degrees = [];
 
-            foreach($request->degrees as $degree) {
+            foreach ($request->degrees as $degree) {
                 $deg = new Degree();
                 $deg->title = $degree['title'];
                 $deg->received = $degree['received'];
@@ -124,16 +127,16 @@ class UserController extends Controller
                 'user' => $request->user()
             ]);
         }
-    }   
+    }
 
-     /**
+    /**
      * Handle an authentication attempt.
      *
      * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function authenticate(Request $request)
-    {   
+    {
         $validated = $request->validate([
             'email' => 'email|required|exists:users',
             'password' => 'required|string|min:6'
@@ -153,7 +156,7 @@ class UserController extends Controller
             'errors' => [
                 'password' => ['The provided credentials do not match our records.'],
             ]
-        ],422);
+        ], 422);
     }
 
     /**
@@ -171,13 +174,14 @@ class UserController extends Controller
         $request->session()->regenerateToken();
 
         return response()->json([
-            'success'=>true
+            'success' => true
         ]);
     }
 
-    public function getAllUsers(Request $request) {
+    public function getAllUsers(Request $request)
+    {
 
-        if ( $request->user()->type != 'admin') 
+        if ($request->user()->type != 'admin')
             return response()->json([
                 'error' => true,
                 'message' => 'You do not have the authority to perform this action'
@@ -188,9 +192,10 @@ class UserController extends Controller
         ]);
     }
 
-    public function getAllUsersOfType(Request $request, $type) {
-        
-        if ( $request->user()->type == 'viewer' )
+    public function getAllUsersOfType(Request $request, $type)
+    {
+
+        if ($request->user()->type == 'viewer')
             return response()->json([
                 'error' => true,
                 'message' => 'You do not have the authority to perform this action'
@@ -198,27 +203,29 @@ class UserController extends Controller
 
 
         return response()->json([
-            'users' => User::where('type',$type)->get()
+            'users' => User::where('type', $type)->get()
         ]);
     }
 
-    public function getUserById(Request $request, $id) {
+    public function getUserById(Request $request, $id)
+    {
 
-        if ( $request->user()->type != 'admin'  && $request->user()->type != 'editor') 
+        if ($request->user()->type != 'admin'  && $request->user()->type != 'editor')
+            return response()->json([
+                'error' => true,
+                'message' => 'You do not have the authority to perform this action'
+            ], 401);
+
+
         return response()->json([
-            'error' => true,
-            'message' => 'You do not have the authority to perform this action'
-        ], 401);
-
-
-        return response()->json([
-            'user' => User::where('id',$id)->first()
+            'user' => User::where('id', $id)->first()
         ]);
     }
 
-    public function addUser(Request $request) {
+    public function addUser(Request $request)
+    {
 
-        if ( $request->user()->type != 'admin') 
+        if ($request->user()->type != 'admin')
             return response()->json([
                 'error' => true,
                 'message' => 'You do not have the authority to perform this action'
@@ -236,17 +243,17 @@ class UserController extends Controller
             'status' => 'required|in:approved,awaiting'
         ]);
 
-        if ( $request->type === "researcher" ) {
+        if ($request->type === "researcher") {
             $index = 0;
-            $errors = [];        
-            foreach ( $request->degrees as $degree) {                                
+            $errors = [];
+            foreach ($request->degrees as $degree) {
                 $validator = Validator::make($degree, [
-                    'title'=>'required|string',
-                    'institution'=>'required|string',
-                    'received'=>'required|date'
+                    'title' => 'required|string',
+                    'institution' => 'required|string',
+                    'received' => 'required|date'
                 ]);
-                if ( $validator->fails()) 
-                    $errors['degree_'.$index] = 
+                if ($validator->fails())
+                    $errors['degree_' . $index] =
                         $validator->errors();
 
                 $index++;
@@ -254,11 +261,10 @@ class UserController extends Controller
 
             if (count($errors) > 0) {
                 return response()->json([
-                    "message"=>"The given data was invalid",
-                    "errors"=> $errors
+                    "message" => "The given data was invalid",
+                    "errors" => $errors
                 ], 422);
-            }      
-
+            }
         }
 
         $newUser = new User;
@@ -271,10 +277,10 @@ class UserController extends Controller
         $newUser->status = $request->status;
         $newUser->save();
 
-        if ($request->type == "researcher"){
+        if ($request->type == "researcher") {
             $degrees = [];
 
-            foreach($request->degrees as $degree) {
+            foreach ($request->degrees as $degree) {
                 $deg = new Degree();
                 $deg->title = $degree['title'];
                 $deg->received = $degree['received'];
@@ -297,26 +303,27 @@ class UserController extends Controller
         ]);
     }
 
-    public function editUser(Request $request, $id) {
+    public function editUser(Request $request, $id)
+    {
 
 
-        if ( $request->user()->type != 'admin') 
+        if ($request->user()->type != 'admin')
             return response()->json([
                 'error' => true,
                 'message' => 'You do not have the authority to perform this action'
             ], 401);
 
-        
+
         $user = User::find($id);
-        
-        if (!$user) 
-        return response()->json([
-            'error' => true,
-            'message' => 'User with id ' . $id . ' does not exist'
-        ],404);
+
+        if (!$user)
+            return response()->json([
+                'error' => true,
+                'message' => 'User with id ' . $id . ' does not exist'
+            ], 404);
 
         $validated = $request->validate([
-            'email' => 'email|exclude_if:email,'.$user->email .'|unique:users',
+            'email' => 'email|exclude_if:email,' . $user->email . '|unique:users',
             'password' => 'string|min:6',
             'first_name' => 'string',
             'last_name' => 'string',
@@ -328,16 +335,16 @@ class UserController extends Controller
 
         if ($request->has('email'))
             $user->email = $request->email;
-        
+
         if ($request->has('password'))
             $user->password = $request->password;
-        
+
         if ($request->has('first_name'))
             $user->first_name = $request->first_name;
 
         if ($request->has('last_name'))
             $user->last_name = $request->last_name;
-        
+
         if ($request->has('type'))
             $user->type = $request->type;
 
@@ -346,16 +353,17 @@ class UserController extends Controller
 
         if ($request->has('status'))
             $user->status = $request->status;
-        
+
         $user->save();
-        
+
         return response()->json([
             'success' => true,
         ]);
     }
 
-    public function changeStatus(Request $request, $id) {
-        if ( $request->user()->type != 'admin' &&  $request->user()->type != 'editor') 
+    public function changeStatus(Request $request, $id)
+    {
+        if ($request->user()->type != 'admin' &&  $request->user()->type != 'editor')
             return response()->json([
                 'error' => true,
                 'message' => 'You do not have the authority to perform this action'
@@ -363,35 +371,36 @@ class UserController extends Controller
 
         $user = User::find($id);
 
-        if (!$user) 
+        if (!$user)
             return response()->json([
                 'error' => true,
                 'message' => 'User with id ' . $id . ' does not exist'
-            ],404);
+            ], 404);
 
         $validated = $request->validate([
             'status' => 'in:awaiting,approved'
         ]);
-        
+
         $user->status = 'approved';
         $user->save();
-        
+
         return response()->json([
             'success' => true,
         ]);
     }
 
-    public function removeUser(Request $request, $id) {
+    public function removeUser(Request $request, $id)
+    {
 
-        if ( $request->user()->type != 'admin' && $request->user()->type != 'editor' ) 
+        if ($request->user()->type != 'admin' && $request->user()->type != 'editor')
             return response()->json([
                 'error' => true,
                 'message' => 'You do not have the authority to perform this action'
             ], 401);
-            
+
         $user = User::find($id);
-        
-        if (!$user) 
+
+        if (!$user)
             return response()->json([
                 'error' => true,
                 'message' => 'User with id ' . $id . ' does not exist'
@@ -404,45 +413,45 @@ class UserController extends Controller
         ]);
     }
 
-    public function getUserDegrees(Request $request, $id) {
+    public function getUserDegrees(Request $request, $id)
+    {
 
         $user = User::find($id);
-        
-        if (!$user) 
+
+        if (!$user)
             return response()->json([
                 'error' => true,
                 'message' => 'User with id ' . $id . ' does not exist'
             ]);
 
-        if ($user->type != 'researcher') 
+        if ($user->type != 'researcher')
             return response()->json([
                 'error' => true,
                 'message' => 'User with id ' . $id . ' is not a researcher'
             ]);
-        
-        
+
+
         $degrees = Degree::where('researcher_email', $user->email)->get();
 
         return response()->json([
             'degrees' => $degrees
         ]);
-
-
     }
-    
 
-    public function addDegree(Request $request, $id) {
 
-        if ( $request->user()->type != 'admin' && $request->user()->id != id) 
+    public function addDegree(Request $request, $id)
+    {
+
+        if ($request->user()->type != 'admin' && $request->user()->id != $id)
             return response()->json([
                 'error' => true,
                 'message' => 'You do not have the authority to perform this action'
-           ], 401);
+            ], 401);
 
         $request->validate([
-            'title'=>'required|string',
-            'institution'=>'required|string',
-            'received'=>'required|string',
+            'title' => 'required|string',
+            'institution' => 'required|string',
+            'received' => 'required|string',
         ]);
 
         $user = User::find($id);
@@ -450,8 +459,8 @@ class UserController extends Controller
         if (!$user) {
             return response()->json([
                 'error' => true,
-                'message' => 'User with id '.$id.' does not exist'
-            ],404);
+                'message' => 'User with id ' . $id . ' does not exist'
+            ], 404);
         }
 
         $deg = new Degree;
@@ -464,18 +473,18 @@ class UserController extends Controller
         return response()->json([
             'success' => true,
         ]);
-
     }
 
-    public function removeDegree(Request $request, $id) {
-        if ( $request->user()->type != 'admin' && $request->user()->id != id) 
+    public function removeDegree(Request $request, $id)
+    {
+        if ($request->user()->type != 'admin' && $request->user()->id != $id)
             return response()->json([
                 'error' => true,
                 'message' => 'You do not have the authority to perform this action'
-           ], 401);
+            ], 401);
 
         $request->validate([
-            'degree_id'=>'required|exists:degrees,id',
+            'degree_id' => 'required|exists:degrees,id',
         ]);
 
 
@@ -485,7 +494,7 @@ class UserController extends Controller
         if (!$user) {
             return response()->json([
                 'error' => true,
-                'message' => 'User with id '.$id.' does not exist'
+                'message' => 'User with id ' . $id . ' does not exist'
             ]);
         }
 
@@ -494,7 +503,7 @@ class UserController extends Controller
         if (!$degree)
             return response()->json([
                 'error' => true,
-                'message' => 'Degree with id '.$request->degree_id.' does not exist'
+                'message' => 'Degree with id ' . $request->degree_id . ' does not exist'
             ]);
 
         $degree->delete();
@@ -505,12 +514,13 @@ class UserController extends Controller
     }
 
 
-    public function getSignUps(Request $request) {
-        if ( $request->user()->type != 'admin'  && $request->user()->type != 'editor' ) 
+    public function getSignUps(Request $request)
+    {
+        if ($request->user()->type != 'admin'  && $request->user()->type != 'editor')
             return response()->json([
                 'error' => true,
                 'message' => 'You do not have the authority to perform this action'
-           ], 401);
+            ], 401);
 
         return response()->json([
             'users' => User::where('status', 'awaiting')->get()
